4b1e49cbb79367bb52d1e20372ce141a6f5f98de
Kalle <khirvola@gmail.com>
2018-07-24 13:44:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Project Title
+
+Bulk converts mp3 albums to mp4 files
+
+## Getting Started
+
+### Prerequisites
+
+* Python 2.7
+* [FFmpeg](https://www.ffmpeg.org/) (Windows executable included in repo)
+
+### Usage
+
+1. Create directory called `mp3` to project root.
+2. Fill it with albums. Albums should have image file (.jpg/.png) and at least one .mp3 file in it.
+
+Example folder structure:
+
+```
+Project
+|
++-- main.py
++-- ffmpeg.exe
+|
++-- mp3/
+    |
+    +-- Album1/
+    |   |
+    |   +-- Song1.mp3
+    |   +-- Song2.mp3
+    |   +-- Song3.mp3
+    |   +-- AlbumArt.jpg
+    |
+    +-- Album2/
+    |   |
+    |   +-- Song1.mp3
+    |   +-- Song2.mp3
+    |   +-- Song3.mp3
+    |   +-- Art.png
+    |
+    +-- Album3/
+        |
+        +-- Song1.mp3
+        +-- Song2.mp3
+        +-- Song3.mp3
+        +-- SomeArt.jpg
+```
+
+3. Run the conversion by typing `python main.py`. Files converted will go to `./mp4` folder.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ main.py
@@ -0,0 +1,81 @@
+import subprocess
+from mimetypes import MimeTypes
+from os import devnull, getcwd, listdir, makedirs, walk
+from os.path import basename, dirname, exists, isfile, join, splitext
+from pprint import pprint
+from urllib import pathname2url
+
+ALLOWED_AUDIO_MIMETYPES = ['audio/mpeg']
+ALLOWED_IMAGE_MIMETYPES = ['image/jpeg', 'image/png']
+
+CWD = getcwd()
+MP3_DIR = join(CWD, 'mp3')
+
+# Setup necessary variables
+mime = MimeTypes()
+
+def get_mp3_dirs():
+  dirs = next(walk(MP3_DIR))[1]
+  return map(lambda d: join(MP3_DIR, d), dirs)
+
+def get_mp3_files(dir_path):
+  files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+  full_paths = map(lambda f: join(dir_path, f), files)
+  mp3_files = filter(lambda f: (get_mime_type(f) in ALLOWED_AUDIO_MIMETYPES), full_paths)
+  return mp3_files
+
+def get_image_file(dir_path):
+  files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+  full_paths = map(lambda f: join(dir_path, f), files)
+
+  for file_path in full_paths:
+    if get_mime_type(file_path) in ALLOWED_IMAGE_MIMETYPES:
+      return file_path
+  
+  return False
+
+def get_mime_type(file_path):
+  url = pathname2url(file_path)
+  mime_type = mime.guess_type(url)
+  return mime_type[0]
+
+def convert_to_mp4(file_path, image_path):
+  dir_name =  basename(dirname(file_path))
+  file_name = splitext(basename(file_path))[0]
+  target_dir = join(CWD, 'mp4', dir_name)
+  target_path = join(target_dir, file_name) + '.mp4'
+
+  if not exists(target_dir):
+    print 'Directory doesn\'t exist. Creating "%s"' % (target_dir)
+    makedirs(target_dir)
+
+  print 'Processing "%s"' % (file_name)
+  
+  args = ['ffmpeg', '-loglevel', 'panic', '-y',
+          '-loop', '1', '-r', '1',
+          '-i', image_path,
+          '-i', file_path,
+          '-c:a', 'copy', '-vf', 'scale=720:-2',
+          '-shortest', target_path]
+
+  subprocess.call(args)
+
+def main():
+  dirs = get_mp3_dirs()
+
+  for d in dirs:
+    image_file = get_image_file(d)
+
+    if not image_file:
+      print 'Could not find image file for "%s". Continuing...' % (d)
+      continue
+
+    mp3_files = get_mp3_files(d)
+
+    for mp3_file in mp3_files:
+      convert_to_mp4(mp3_file, image_file)
+
+  print 'Absolutely DONE.'
+
+if __name__ == '__main__':
+  main()
